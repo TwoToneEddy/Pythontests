47cb34e13dc4596ec10025b7a85555507e6d8400
lee <lee.hudson1384@yahoo.co.uk>
2022-02-27 19:18:23 +0000

    Crappy shite

--- ledPlacement.py
+++ ledPlacement.py
@@ -14,37 +14,110 @@ leftToRightY = 0.18
 colDX = 17.38
 rowDY = 7.44
 
+transistorDX = 0
+transistorDY = -6
+
+baseRDX = 4.826 - xOrigin
+baseRDY = -10
+
+emRDX = 6.731 - xOrigin
+emRDY = -10
+
 xCoords = list()
 yCoords = list()
 
 xTicks = list()
 yTicks = list()
+
+ledComponentNo = 1
+transComponentNo = 101
+baseResComponentNo = 101
+emResComponentNo = 201
+
+
+globalComponentPrefix = "L"
+
+output = """
+var canvasConfig = api('editorCall', {cmd:'canvas_config'});
+
+
+console.clear();
+var s = api('getSource', {type: "json"});
+var LEDs = [];
+for (let x in s.FOOTPRINT){
+	for(let i in s.FOOTPRINT[x].TEXT){
+		var id = s.FOOTPRINT[x].TEXT;
+		var name = s.FOOTPRINT[x].TEXT[i].text;
+		if(name.slice(0,2)=="R2"){
+			LEDs.push([name,x]);
+		}
+	}
+}
+
+LEDs.forEach(myFunc);
+
+function myFunc(value,index){
+    if(value[0] == "R201"){
+        api('moveObjsTo', {objs:[{gId:value[1]}], x:s.canvas.originX + api('valConvert', {type:'real2canvas',val:'6.731mm'}), y:s.canvas.originY+api('valConvert', {type:'real2canvas',val:'10.0mm'})});
+    }
+}
+"""
+
+
+
+def formatPrint(prefix,componentNo,x,y):
+    y = y*-1
+    print(f'if(value[0] == "{prefix}{componentNo}"){{')
+    print(f"\tapi('moveObjsTo', {{objs:[{{gId:value[1]}}], x:s.canvas.originX + api('valConvert', {{type:'real2canvas',val:'{x}mm'}}), y:s.canvas.originY+api('valConvert', {{type:'real2canvas',val:'{y}mm'}})}});")
+    print("}")
+    #print(f"{prefix}{componentNo}\n{x}\n{y*-1}")
+
+def printCoords(x,y):
+    global ledComponentNo
+    global transComponentNo
+    global baseResComponentNo
+    global emResComponentNo
+    #formatPrint("L",ledComponentNo,x,y)
+    #formatPrint("Q",transComponentNo,x+transistorDX,y+transistorDY)
+    #formatPrint("R",baseResComponentNo,x+baseRDX,y+baseRDY)
+    formatPrint("R",emResComponentNo,x+emRDX,y+emRDY)
+
+    """
+    print(f"L{ledComponentNo} -> X:{x} Y:{y}")
+    print(f"Q{transComponentNo} -> X:{x+transistorDX} Y:{y+transistorDY}")
+    print(f"R{baseResComponentNo} -> X:{x+baseRDX} Y:{y+baseRDY}")
+    print(f"R{emResComponentNo} -> X:{x+emRDX} Y:{y+emRDY}")
+    """
+    ledComponentNo+=1
+    transComponentNo+=1
+    baseResComponentNo+=1
+    emResComponentNo+=1
+
 def printCluster(col,row):
+    global ledComponentNo
     ledLX = (col*colDX)+xOrigin
     ledLY = (row * -7.44)+yOrigin
     xCoords.append(ledLX)
     xTicks.append(ledLX)
     yCoords.append(ledLY)
     yTicks.append(ledLY)
+    printCoords(ledLX,ledLY)
 
     ledMX = ledLX + leftToMidX
     ledMY = ledLY + leftToMidY
     xCoords.append(ledMX)
     yCoords.append(ledMY)
+    printCoords(ledMX,ledMY)
 
     ledRX = ledLX + leftToRightX
     ledRY = ledLY + leftToRightY
     xCoords.append(ledRX)
     yCoords.append(ledRY)
-    print(f"***** Col {col}, row {row} ******")
-    print(f"{ledLX},{ledLY}")
-    print(f"{ledMX},{ledMY}")
-    print(f"{ledRX},{ledRY}")
-    print(f"*********************************")
-    print()
+    printCoords(ledRX,ledRY)
 
 
 printCluster(0,0)
+
 printCluster(0,2)
 printCluster(0,4)
 printCluster(0,6)
@@ -75,6 +148,7 @@ printCluster(6,0)
 printCluster(6,2)
 printCluster(6,4)
 printCluster(6,6)
+
 #plt.scatter(xCoords,yCoords)
 #plt.xlim(-5,30)
 #plt.ylim(-5,30)
@@ -93,5 +167,5 @@ ax.set_xticks(xTicks)
 ax.set_yticks(yTicks)
 plt.xlim(0,125)
 plt.ylim(-60,1)
-plt.show()
+#plt.show()
 
